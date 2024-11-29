1dc5068ded1ef336511a852b1332631c91814e51
Akshat Bamania <akshat.bamania23@gmail.com>
2024-11-29 23:31:59 +0530

    second commit

--- index.js
+++ index.js
@@ -25,7 +25,41 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User",userSchema)
 
-const user1 = new User({name:"Kanishk" , Email : "Kyu@bhai.in" , Age: 12 })
+// const user1 = new User({name:"Kanishk" , Email : "Kyu@bhai.in" , Age: 12 })
 
-user1.save()
 
+
+// const user2 = new User({name:"Bhola", Email:"tanjiro@slayer.com", Age:19 })
+
+// user2.save()
+//     .then((res)=>{
+//         console.log(res);
+        
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+        
+//     });
+
+
+User.insertMany([
+        {
+            Name:"Tony",
+            Email:"saynototony@gmail.com",
+            Age:12
+    },
+
+    {
+        name:"Yahoo",
+        Email:"Yahoo@gmiail.in",
+        Age:21
+    },
+])
+.then((res)=>{
+    console.log(res);
+    
+})
+.catch((err)=>{
+    console.log(err);
+    
+})
